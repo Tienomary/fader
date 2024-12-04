07f491e242cac68fbfcb7bb7a318188d7f2712e7
Marine_rst <marine.meudon@gmail.com>
2024-12-04 08:50:05 +0100

    J'ai enlevé le couche de fully connected et j'ai changé le décoder et les formes des relu

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class CelebADataset(Dataset):
     def __init__(self, img_dir, attr_path, partition_path, transform=None, split="train"):
@@ -32,6 +33,10 @@ class CelebADataset(Dataset):
         attr = self.attributes.loc[img_name].values.astype("float32")
         return image, attr
 
+    def get_attribute_names(self):
+        # Retourner les noms des attributs en excluant la colonne 'image_id'
+        return self.attributes.columns.tolist()
+
 transform = transforms.Compose([
     transforms.Resize((256,256)),
     transforms.ToTensor(),
@@ -50,9 +55,30 @@ print(f"Nombre d'images dans le dataset : {len(dataset)}")
 batch_size = 32
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
+
+
+# afficher une image et ses attributs
+def affiche(dataset, image, attributs):
+    attribute_names = dataset.get_attribute_names()
+
+    # Afficher l'image
+    image = image.permute(1, 2, 0).numpy()  
+    plt.imshow(image)
+    plt.axis('off')  
+    plt.show()
+
+    # Afficher les attributs associés à l'image
+    print(f"Attributs pour cette image :")
+    for i, attr in enumerate(attributs):
+        print(f"{attribute_names[i]} : {'Oui' if attr.item() == 1 else 'Non'}")
+
+
 for images, attrs in dataloader:
     print(images.shape) 
-    print(attrs.shape)  
+    print(attrs.shape)
+    image = images[0] 
+    attributs = attrs[0]
+    affiche(dataset, image, attributs)
     break
 
 class Encoder(nn.Module):
@@ -60,62 +86,82 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2),
             nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=False),
+            nn.LeakyReLU(0.2)
         )
-        self.fc = nn.Linear(2048, latent_dim)
+
 
     def forward(self, x):
-        print('Inti : ', x.shape)
+        print('Init : ', x.shape)
         x = self.model(x)
-        print('befo flaaten encoder : ', x.shape)
-        x = self.fc(x.view(x.size(0), -1))
-        print('sortie encoder : ', x.shape)
+        print('latent shape : ', x.shape)
         return x
 
 class Decoder(nn.Module):
     def __init__(self, latent_dim=512, num_attributes=40):
         super(Decoder, self).__init__()
-        self.fc = nn.Linear(latent_dim + num_attributes, 2048)
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(inplace=False),
-            nn.ConvTranspose2d(16, 3, kernel_size=4, stride=2, padding=1),
-            nn.ReLU()
-        )
+        self.num_attributes = num_attributes
+        self.latent_dim = 2 * num_attributes
+        self.deconv1 = nn.ConvTranspose2d(512 + self.latent_dim, 512, kernel_size=4, stride=2, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(512 + self.latent_dim, 256, kernel_size=4, stride=2, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(256 + self.latent_dim, 128, kernel_size=4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(128 + self.latent_dim, 64, kernel_size=4, stride=2, padding=1)
+        self.deconv5 = nn.ConvTranspose2d(64 + self.latent_dim, 32, kernel_size=4, stride=2, padding=1)
+        self.deconv6 = nn.ConvTranspose2d(32 + self.latent_dim, 16, kernel_size=4, stride=2, padding=1)
+        self.deconv7 = nn.ConvTranspose2d(16 + self.latent_dim, 3, kernel_size=4, stride=2, padding=1)
+        
 
-    def forward(self, z, attributes):
-        z = torch.cat([z, attributes], dim=1)
-        print('Entree decoder avec attributs en + : ',z.shape)
-        z = self.fc(z)
-        print(z.shape)
-        z = z.view(z.size(0), 512, 2, 2)
-        print(z.shape)
-        z = self.model(z)
-        print(z.shape)
+    def forward(self, z, attributs):
+
+        print('latent shape : ', z.shape)
+        latent_code = attributs.view(batch_size, self.latent_dim, 1, 1)  # Reshape latent code.
+
+
+        # Append latent code to the feature maps at every layer.
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv1(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv2(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv3(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv4(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv5(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv6(z))
+
+        z = torch.cat([z, latent_code], dim=1)
+        z = self.relu(self.deconv7(z)) 
+        print('Image shape : ', x.shape)
         return z
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()  # Match latent_dim with Encoder's output size
+
+    def forward(self, x, attr):
+        z = self.encoder(x)
+        output_image = self.decoder(z, attr)
+        return output_image
     
 class Discriminator(nn.Module):
     def __init__(self, latent_dim=512, num_attributes=40):
@@ -139,48 +185,39 @@ class Discriminator(nn.Module):
         return z
     
 
+torch.autograd.set_detect_anomaly(True)
 
+autoencoder = AutoEncoder()
+optimizer_autoencoder = optim.Adam(autoencoder.parameters(), lr=0.001)
 
-encoder = Encoder()
-print(encoder)
-optimizer_encoder = optim.Adam(encoder.parameters(), lr=0.001)
-
-decoder = Decoder()
-print(decoder)
-optimizer_decoder = optim.Adam(decoder.parameters(), lr=0.001)
-
-discriminator = Discriminator()
-print(discriminator)
-optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=0.001)
+#discriminator = Discriminator()
+#print(discriminator)
+#optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=0.001)
 
 reconstruction_loss = nn.MSELoss()
-discriminator_loss = nn.BCELoss()
+#discriminator_loss = nn.BCELoss()
 
 num_epochs=10
 for epoch in range(num_epochs):
     for images, attributs in dataloader:
-        # Encode
-        E = encoder(images)
-        # Decode
-        R = decoder(E, attributs)
+        # Autoencoder
+        pred_images = autoencoder(images, attributs)
 
-        recon_loss = reconstruction_loss(R, images)
+        recon_loss = reconstruction_loss(pred_images, images)
 
-        #ERREUR PAR ICI 
 
         pred_attributes = discriminator(E.view(32, 512, 1, 1))
-        disc_loss = discriminator_loss(pred_attributes, attributs)
+#        disc_loss = discriminator_loss(pred_attributes, attributs)
 
-        lambda_adv = 0.0001
-        encoder_loss = recon_loss - lambda_adv * disc_loss
+#        lambda_adv = 0.0001
+#        encoder_loss = recon_loss - lambda_adv * disc_loss
 
-        optimizer_encoder.zero_grad()
-        optimizer_decoder.zero_grad()
-        recon_loss.backward(retain_graph=True)
+        optimizer_autoencoder.zero_grad()
+        recon_loss.backward()
 
-        optimizer_encoder.step()
-        optimizer_decoder.step()
-        optimizer_discriminator.zero_grad()
-        disc_loss.backward()
-        optimizer_discriminator.step()
+        optimizer_autoencoder.step()
+
+ #       optimizer_discriminator.zero_grad()
+#        disc_loss.backward()
+ #       optimizer_discriminator.step()
 
