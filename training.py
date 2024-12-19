9681bd95a8b219c5c586ffbcdd000b32e708cb0f
Tienomary <37733721+Tienomary@users.noreply.github.com>
2024-12-19 16:44:43 +0100

    tr

--- training.py
+++ training.py
@@ -15,22 +15,24 @@ from model import *
 torch.autograd.set_detect_anomaly(True)
 
 autoencoder = AutoEncoder()
+discriminator = Discriminator()
+encoder = Encoder()
 
 
-optimizer_autoencoder = optim.Adam(autoencoder.parameters(), lr=0.001)
+optimizer_autoencoder = optim.Adam(encoder.parameters(), lr=0.001)
 
-lambda=0
+lambdaa=0
 
 reconstruction_loss=nn.MSELoss()
 discriminator_loss=nn.BCELoss()
-adversarial_loss = reconstrucion_loss-lambda*discriminator_loss
 
 num_epochs = 10
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+encoder.to(device)
 autoencoder.to(device)
-autoencoder.load_state_dict(torch.load("autoencoder.pth"))
+discriminator.to(device)
 
 for epoch in range(num_epochs):
     print(f"Epoch {epoch + 1}/{num_epochs}")
@@ -43,17 +45,17 @@ for epoch in range(num_epochs):
         pred_images = autoencoder(images, attributs)
         latent_images = encoder(images)
         pred_attributs = discriminator(latent_images)
-        inv_attributs= torch.ones(32,40)-attributs
+        inv_attributs= torch.ones(32,40).to(device)-attributs
         inv_attributs= inv_attributs.to(device)
-        lambda += 0.0001 / 500000
-        adv_loss= reconstruction_loss(pred_images,images) - lambda*discriminator_loss(pred_attributs,inv_attributs)
+        lambdaa += 0.0001 / 500000
+        adv_loss= reconstruction_loss(pred_images,images) - lambdaa*discriminator_loss(pred_attributs,inv_attributs)
       
         
         optimizer_autoencoder.zero_grad()
         adv_loss.backward()
         optimizer_autoencoder.step()
-        epoch_loss += recon_loss.item()
+        epoch_loss += adv_loss.item()
 
     epoch_loss /= num_batches
-    torch.save(autoencoder.state_dict(), "autoencoder.pth")
+    torch.save(autoencoder.state_dict(), "encoder.pth")
     print(f"Epoch {epoch + 1}/{num_epochs} - Average Reconstruction Loss: {epoch_loss:.4f}")
