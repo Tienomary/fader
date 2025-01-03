c4d08323e793a041d34a3b9d96647326069b65f0
Tienomary <37733721+Tienomary@users.noreply.github.com>
2025-01-03 15:41:02 +0100

    pour OVH

--- training.py
+++ training.py
@@ -95,11 +95,17 @@ for epoch in range(num_epochs):
             recon_images = autoencoder.decoder(latent_images, attributs)
             recon_loss = reconstruction_loss(images, recon_images)
             ae_val_loss += recon_loss.item()
+    with open('vect_train_ae_loss.txt', 'w') as f:
+        f.write("%s\n" % str(ae_train_loss / len(dataloader_train)))
 
-    vect_train_ae_loss.append(ae_train_loss / len(dataloader_train))
-    vect_train_disc_loss.append(disc_train_loss / len(dataloader_train))
-    vect_val_ae_loss.append(ae_val_loss / len(dataloader_val))
-    vect_val_disc_loss.append(disc_val_loss / len(dataloader_val))
+    with open('vect_train_disc_loss.txt', 'w') as f:
+        f.write("%s\n" % str(disc_train_loss / len(dataloader_train)))
+
+    with open('vect_val_ae_loss.txt', 'w') as f:
+       f.write("%s\n" % str(ae_val_loss / len(dataloader_val)))
+
+    with open('vect_val_disc_loss.txt', 'w') as f:
+        f.write("%s\n" % str(disc_val_loss / len(dataloader_val)))
 
     torch.save(discriminator.state_dict(), "discriminator.pth")
     torch.save(autoencoder.state_dict(), "autoencoder.pth")
