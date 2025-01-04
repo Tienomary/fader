a45dbf7d633736567d4f882fed049027642107f1
Tienomary <37733721+Tienomary@users.noreply.github.com>
2025-01-04 10:54:15 +0100

    courbes

--- train_sexe/courbe.py
+++ train_sexe/courbe.py
@@ -6,10 +6,10 @@ def read_losses(file_path):
     return [float(line.strip()) for line in lines]
 
 # Chemins vers les fichiers
-train_ae_loss_file = "vect_train_ae_loss.txt"
-train_disc_loss_file = "vect_train_disc_loss.txt"
-val_ae_loss_file = "vect_val_ae_loss.txt"
-val_disc_loss_file = "vect_val_disc_loss.txt"
+train_ae_loss_file = "train_sexe/vect_train_ae_loss.txt"
+train_disc_loss_file = "train_sexe/vect_train_disc_loss.txt"
+val_ae_loss_file = "train_sexe/vect_val_ae_loss.txt"
+val_disc_loss_file = "train_sexe/vect_val_disc_loss.txt"
 
 # Lecture des pertes
 train_ae_loss = read_losses(train_ae_loss_file)
@@ -19,9 +19,9 @@ val_disc_loss = read_losses(val_disc_loss_file)
 
 # Tracer les courbes
 plt.figure(figsize=(10, 6))
-plt.plot(train_ae_loss, label='Train AE Loss', linestyle='-', marker='o')
+#plt.plot(train_ae_loss, label='Train AE Loss', linestyle='-', marker='o')
 plt.plot(train_disc_loss, label='Train Disc Loss', linestyle='-', marker='o')
-plt.plot(val_ae_loss, label='Validation AE Loss', linestyle='--', marker='s')
+#plt.plot(val_ae_loss, label='Validation AE Loss', linestyle='--', marker='s')
 plt.plot(val_disc_loss, label='Validation Disc Loss', linestyle='--', marker='s')
 
 plt.xlabel('Epochs')
